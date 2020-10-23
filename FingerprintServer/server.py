3f9fe2af55bbffb0721aead576b7ea4b242586c1
Gordex2014 <alvaromsd_11@hotmail.com>
2020-10-23 19:07:48 -0400

    Correcciones menores relacionadas a la identación

--- FingerprintServer/server.py
+++ FingerprintServer/server.py
@@ -24,56 +24,58 @@ from flask import Flask, jsonify, request
 from pyfingerprint.pyfingerprint import PyFingerprint
 
 
-
-## Agrega una nueva huella dactilar
+# Agrega una nueva huella dactilar
 ##
 
 app = Flask(__name__)
 
+
 @app.route('/enroll')
 def enroll_user():
 
-    ## Intenta inicializar el sensor
+    # Intenta inicializar el sensor
 
     try:
         f = PyFingerprint('/dev/ttyUSB0', 57600, 0xFFFFFFFF, 0x00000000)
 
-        if ( f.verifyPassword() == False ):
-            raise ValueError('La contraseña proporcionada para el sensor es erronea!')
+        if (f.verifyPassword() == False):
+            raise ValueError(
+                'La contraseña proporcionada para el sensor es erronea!')
 
     except Exception as e:
         print('El sensor de huellas dactilares no se pudo inicializar!')
         print('Mensaje de excepcion: ' + str(e))
         exit(1)
 
-    ## Intenta ingresar nuevos datos al sensor
+    # Intenta ingresar nuevos datos al sensor
     try:
         send_instructions('Ponga el dedo en el sensor')
 
-        ## Espera a que la imagen sea leída
-        while ( f.readImage() == False ):
+        # Espera a que la imagen sea leída
+        while (f.readImage() == False):
             pass
 
-        ## Convierte la imagen leída a características y la almacena en el buffer 1
+        # Convierte la imagen leída a características y la almacena en el buffer 1
         f.convertImage(0x01)
 
-        ## Se obtienen las características de la base de datos
+        # Se obtienen las características de la base de datos
         characteristics_data = retrieve_characteristics()
         for characteristic in characteristics_data:
-            ## Si la lista llega vacía, quiere decir que la información que llegó corresponde al votante que no tiene la
-            ## huella registrada, así que se omite esta prueba 
+            # Si la lista llega vacía, quiere decir que la información que llegó corresponde al votante que no tiene la
+            # huella registrada, así que se omite esta prueba
             if len(characteristic) == 0:
                 pass
             else:
-                ## Se sube una por una las listas con las características al buffer 2 para luego
-                ## compararlas con la imagen adquirida
+                # Se sube una por una las listas con las características al buffer 2 para luego
+                # compararlas con la imagen adquirida
                 f.uploadCharacteristics(0x02, characteristic)
 
                 compare_characteristics_payload = f.compareCharacteristics()
-                
-                ## Umbral de decision para la aceptación de huellas dactilares
+
+                # Umbral de decision para la aceptación de huellas dactilares
                 if compare_characteristics_payload > 20:
-                    send_instructions('La huella ya se encuentra registrada en la base de datos')
+                    send_instructions(
+                        'La huella ya se encuentra registrada en la base de datos')
                     return jsonify(
                         value='',
                         error='La huella ya se encuentra registrada en la base de datos'
@@ -84,15 +86,15 @@ def enroll_user():
 
         send_instructions('Ponga el dedo en el sensor nuevamente')
 
-        ## Espera a que la imagen del dedo sea leída nuevamente
-        while ( f.readImage() == False ):
+        # Espera a que la imagen del dedo sea leída nuevamente
+        while (f.readImage() == False):
             pass
 
-        ## Convierte la imagen leída a características y la almacena en el buffer 2
+        # Convierte la imagen leída a características y la almacena en el buffer 2
         f.convertImage(0x02)
 
-        ## Compara los buffers 1 y 2
-        if ( f.compareCharacteristics() == 0 ):
+        # Compara los buffers 1 y 2
+        if (f.compareCharacteristics() == 0):
             # Si las huellas no son iguales se retorna un error
             send_instructions('Las huellas no corresponden')
             return jsonify(
@@ -100,18 +102,18 @@ def enroll_user():
                 error='Las huellas no corresponden'
             )
 
-        ## Se crea la plantilla comparando la informacion almacenada en los buffers 1 y 2
+        # Se crea la plantilla comparando la informacion almacenada en los buffers 1 y 2
         f.createTemplate()
 
-        ## Descarga las características almacenadas en el buffer 1, correspondientes a la plantilla recíen creada
+        # Descarga las características almacenadas en el buffer 1, correspondientes a la plantilla recíen creada
         characteristics = f.downloadCharacteristics(0x01)
 
         send_instructions('Huella registrada correctamente!')
         return jsonify(
-            value=characteristics, ## Se La lista(vector) que contiene las características de la huella
+            # Se La lista(vector) que contiene las características de la huella
+            value=characteristics,
             error=''
         )
-        
 
     except Exception as e:
         print('Operacion fallida!')
@@ -119,24 +121,25 @@ def enroll_user():
         exit(1)
 
 
-## Buscar una huella en el sensor
+# Buscar una huella en el sensor
 ##
 
-@app.route('/search', methods = ['POST'])
+@app.route('/search', methods=['POST'])
 def search_fingerprint():
 
-    ## Se obtiene el número identificador a ser buscado
+    # Se obtiene el número identificador a ser buscado
 
     data = request.json
     characteristics_to_compare = data.get('characteristicsData', '')
 
-    ## Inicialización del sensor
+    # Inicialización del sensor
 
     try:
         f = PyFingerprint('/dev/ttyUSB0', 57600, 0xFFFFFFFF, 0x00000000)
 
-        if ( f.verifyPassword() == False ):
-            raise ValueError('La contraseña proporcionada para el sensor es erronea!')
+        if (f.verifyPassword() == False):
+            raise ValueError(
+                'La contraseña proporcionada para el sensor es erronea!')
 
     except Exception as e:
         print('El sensor de huellas dactilares no se pudo inicializar!')
@@ -148,21 +151,21 @@ def search_fingerprint():
     try:
         send_instructions('Ponga su dedo en el sensor')
 
-        ## Se espera a que el dedo sea posicionado
+        # Se espera a que el dedo sea posicionado
 
-        while ( f.readImage() == False ):
+        while (f.readImage() == False):
             pass
 
-        ## Convierte la imagen leída a características y la almacena en el buffer 1
+        # Convierte la imagen leída a características y la almacena en el buffer 1
 
         f.convertImage(0x01)
 
-        ## Se sube una por una las listas con las características al buffer 2 para luego
-        ## compararlas con la imagen adquirida
+        # Se sube una por una las listas con las características al buffer 2 para luego
+        # compararlas con la imagen adquirida
         f.uploadCharacteristics(0x02, characteristics_to_compare)
 
         compare_characteristics_payload = f.compareCharacteristics()
-                
+
         if compare_characteristics_payload > 50:
             send_instructions('Identificado correctamente')
             return jsonify(
@@ -185,12 +188,13 @@ def search_fingerprint():
 # Se envían las instrucciones para luego interactuar en el frontend
 
 def send_instructions(message):
-    requests.post(config.server_url+'/api/fingerprint/printinstructions', data = {
+    requests.post(config.server_url+'/api/fingerprint/printinstructions', data={
         'instructions': message,
     })
 
-## Función que se encarga de pedir datos de la base de datos relacionadas a las huellas
-## dactilares de todas las personas empadronadas, para se comprobadas
+# Función que se encarga de pedir datos de la base de datos relacionadas a las huellas
+# dactilares de todas las personas empadronadas, para se comprobadas
+
 
 def retrieve_characteristics():
     data = requests.get(config.server_url+'/api/voters/fingercharacteristics')
